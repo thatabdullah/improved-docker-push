4b4efd6be21b8ba0c7fa0c411d8c626c9aa4f98d
thatabdullah <443014855@sm.imamu.edu.sa>
2025-01-23 17:38:37 +0300

    different image digests

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-local_image=$(docker inspect --format='{{.RepoDigests}}' flaskappimg:latest | cut -d '@' -f 2 | tr -d '[]'
- || echo "failed local inspect")
+local_image=$(docker inspect --format='{{.RepoDigests}}' flaskappimg:latest | cut -d '@' -f 2 | tr -d '[]' || echo "failed local inspect")
 echo "$local_image" > /home/ubuntu/app/localimage.txt
 #cut -d '@' -f 2 gets rid of the text before the sha
 #tr -d gets rid of ] in the end of docker inspect's output
